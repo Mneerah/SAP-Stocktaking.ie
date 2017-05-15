ce2446521a933f9f1e892211ef1a9b5cd7e377a0
Mneerah <MoniraAlkaldi@gmail.com>
2017-05-15 14:56:57 +0100

    Update DeleteStockTake.php

--- DeleteStockTake.php
+++ DeleteStockTake.php
@@ -19,7 +19,7 @@
 		DELETE FROM `tblStockTakes` 
 		WHERE `tblStockTakes`.`StockTakeId` ='$ID'";
 
-		$result = mysqli_query($conn, $sql);
+		$result = mssql_query($conn, $sql);
 		if ($conn->query($sql) === TRUE) {
 			//echo "Stocktake Deleted successfully";
 echo "<script>window.close();</script>";
@@ -27,7 +27,7 @@ echo "<script>window.close();</script>";
 		    echo "<h3>Database Error!</h3><h4>You can't delete a stocktake that has staff members assigned.</h4><br> Error updating record: " . $conn->error;
 		}
 		
-	mysqli_close($conn);
+	mssql_close($conn);
 
 ?>
 </body>
