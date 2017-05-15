e900833da440b8927872b70537dfa939576a4f90
Mneerah <MoniraAlkaldi@gmail.com>
2017-05-15 15:27:30 +0100

    Update _ShowStaffSelect.php

--- _ShowStaffSelect.php
+++ _ShowStaffSelect.php
@@ -1,21 +1,16 @@
 <?php
-
 	require ("db_connect.php");
+	$sql =  "SELECT UserId,FirstName,LastName FROM tblUsers;";
+	//add call to user rates and car groups 
 
-		$sql =  "SELECT UserId,FirstName,LastName FROM tblUsers;";
-		//add call to user rates and car groups 
-
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0) {
+	$result = mssql_query($conn, $sql);
+	if (mssql_num_rows($result) > 0) {
 		    // output data of each row
-		    while($row = mysqli_fetch_assoc($result)) {
-				echo'<option id="'. $row["UserId"].'" value="'. $row["UserId"].'">'. $row["FirstName"].' '.$row["LastName"].'</option>';
-		    }
-		} else {
-		    echo "0 results";
-		}
-
-		mysqli_close($conn);
-
-
+	    while($row = mssql_fetch_assoc($result)) {
+		echo'<option id="'. $row["UserId"].'" value="'. $row["UserId"].'">'. $row["FirstName"].' '.$row["LastName"].'</option>';
+	    }
+	} else {
+	    echo "0 results";
+	}
+	mssql_close($conn);
 ?>
